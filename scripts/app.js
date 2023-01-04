8a348248558d5526373841663dad8ff662eab0cc
Hmrhenrique <hmrhenrique2008@gmail.com>
2023-01-04 18:14:24 -0300

    novo codigo aplicado

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 const observerText = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show-text')
+            entry.target.classList.add('show--text')
         } 
     })
 })
-const hiddenTextElements = document.querySelectorAll('.hidden-text')
+const hiddenTextElements = document.querySelectorAll('.hidden--text')
 hiddenTextElements.forEach((el) => observerText.observe(el))
 
 
@@ -13,11 +13,11 @@ hiddenTextElements.forEach((el) => observerText.observe(el))
 const observerIcons = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show-icons')
+            entry.target.classList.add('show--icons')
         } 
     })
 })
-const hiddenIconsElements = document.querySelectorAll('.hidden-icons')
+const hiddenIconsElements = document.querySelectorAll('.hidden--icons')
 hiddenIconsElements.forEach((el) => observerIcons.observe(el))
 
 
@@ -25,9 +25,25 @@ hiddenIconsElements.forEach((el) => observerIcons.observe(el))
 const observerLeftToRight = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show-left-to-right')
+            entry.target.classList.add('show--left--to--right')
         } 
     })
 })
-const hiddenLeftToRightElements = document.querySelectorAll('.hidden-left-to-right')
+const hiddenLeftToRightElements = document.querySelectorAll('.hidden--left--to--right')
 hiddenLeftToRightElements.forEach((el) => observerLeftToRight.observe(el))
+
+var dropdownSwitch = 0
+function dropdown() {
+    dropdownSwitch ++
+    let dropdown = document.querySelector('.nav__dropdown')
+
+    if (dropdownSwitch >= 2){
+        dropdownSwitch = 0
+    }
+
+    if (dropdownSwitch === 0){
+        dropdown.classList.remove('show--dropdown--items')
+    } else {
+        dropdown.classList.add('show--dropdown--items')
+    }
+}
